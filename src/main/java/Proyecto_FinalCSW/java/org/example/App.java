42b7513585cf6a71b704acf247bf7dc1d2ed4b16
lgilucam <lgil@alu.ucam.edu>
2023-01-14 22:19:13 +0100

    first commit

---
+++ src/main/java/Proyecto_FinalCSW/java/org/example/App.java
@@ -0,0 +1,13 @@
+package Proyecto_FinalCSW.java.org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
